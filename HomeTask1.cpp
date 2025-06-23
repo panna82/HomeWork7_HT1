6ae27de383f668d3ce8a46884c9aa1763b3a25b6
Olesya <panna82@mail.ru>
2025-06-24 03:26:26 +0500

    initial

---
+++ HomeTask1.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <string>
+
+using namespace std;
+
+int function(string& str, int length)
+{
+    if (str.length() == length) throw 10;
+    else return str.length();
+}
+
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    string str = "";
+    int length = 0;
+
+    cout << "Введите запретную длину: ";
+    cin >> length;
+
+    while (0 == 0)
+    {
+        try {
+            cout << "Введите слово: ";
+            cin >> str;
+
+            cout << "Длинна слова \"" + str + "\" равна " + to_string(function(str, length)) << endl;
+        }
+        catch (int err)
+        {
+            if (err == 10)
+            {
+                cout << "Вы ввели слово запретной длины! До свидания" << endl;
+                break;
+            }
+        }
+
+    }
+
+}
